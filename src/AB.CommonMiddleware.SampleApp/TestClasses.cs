f2d84f53e9fc18414b5d1f3b8b7a6284306f8797
Andrew <zep519@yahoo.com>
2018-04-29 16:54:14 -0500

    moved docs and added mvc web app

---
+++ src/AB.CommonMiddleware.SampleApp/TestClasses.cs
@@ -0,0 +1,38 @@
+﻿namespace AB.CommonMiddleware.SampleApp
+{
+    public class ScopedClass
+    {
+        private readonly ICorrelationContextAccessor _correlationContext;
+
+        public ScopedClass(ICorrelationContextAccessor correlationContext)
+        {
+            _correlationContext = correlationContext;
+        }
+
+        public string GetCorrelationFromScoped => _correlationContext.CorrelationContext.CorrelationId;
+    }
+
+    public class TransientClass
+    {
+        private readonly ICorrelationContextAccessor _correlationContext;
+
+        public TransientClass(ICorrelationContextAccessor correlationContext)
+        {
+            _correlationContext = correlationContext;
+        }
+
+        public string GetCorrelationFromScoped => _correlationContext.CorrelationContext.CorrelationId;
+    }
+
+    public class SingletonClass
+    {
+        private readonly ICorrelationContextAccessor _correlationContext;
+
+        public SingletonClass(ICorrelationContextAccessor correlationContext)
+        {
+            _correlationContext = correlationContext;
+        }
+
+        public string GetCorrelationFromScoped => _correlationContext.CorrelationContext.CorrelationId;
+    }
+}
