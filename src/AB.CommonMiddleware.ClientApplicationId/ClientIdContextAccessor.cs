c319a892e0b7deb933d07bd9d244ac583a3fb603
Andrew <zep519@yahoo.com>
2018-04-30 19:07:48 -0500

    client id project

---
+++ src/AB.CommonMiddleware.ClientApplicationId/ClientIdContextAccessor.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+
+namespace AB.CommonMiddleware
+{
+    /// <inheritdoc />
+    public class CorrelationContextAccessor : ICorrelationContextAccessor
+    {
+        private static AsyncLocal<CorrelationContext> _correlationContext = new AsyncLocal<CorrelationContext>();
+
+        /// <inheritdoc />
+        public CorrelationContext CorrelationContext
+        {
+            get => _correlationContext.Value;
+            set => _correlationContext.Value = value;
+        }
+    }
+}
---
+++ src/AB.CommonMiddleware.ClientApplicationId/ClientIdServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace AB.CommonMiddleware
+{
+    /// <summary>
+    /// Extensions on the <see cref="IServiceCollection"/>.
+    /// </summary>
+    public static class CorrelationIdServiceExtensions
+    {
+        /// <summary>
+        /// Adds required services to support the Correlation ID functionality.
+        /// </summary>
+        /// <param name="serviceCollection"></param>
+        public static void AddCorrelationId(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.TryAddSingleton<ICorrelationContextAccessor, CorrelationContextAccessor>();
+            serviceCollection.TryAddTransient<ICorrelationContextFactory, CorrelationContextFactory>();
+        }
+    }
+}
