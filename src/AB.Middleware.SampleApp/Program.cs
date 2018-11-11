7b4f64ad9a315ea54b48f45f238d58ab3de070f6
Andrew Brad <zep519@yahoo.com>
2018-11-11 02:44:51 +0000

    Merged PR 19: sample app targets 2.1
    sample app targets 2.1

--- src/AB.Middleware.SampleApp/Program.cs
+++ src/AB.Middleware.SampleApp/Program.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace AB.Middleware.SampleApp
 {
@@ -14,12 +8,13 @@ namespace AB.Middleware.SampleApp
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args)
+                .Build()
+                .Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+                    WebHost.CreateDefaultBuilder(args)
+                        .UseStartup<Startup>();
     }
 }
